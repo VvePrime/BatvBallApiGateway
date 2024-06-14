eaae449e1d7e19b88e41ae22b71a2a8002455f34
VvePrime <vive021172@gmail.com>
2024-06-14 19:27:28 +0530

    initial commit

---
+++ src/main/java/com/vivek/BatvBallApiGateway/BatvBallApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.BatvBallApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatvBallApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatvBallApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivek/BatvBallApiGateway/BatvBallApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.BatvBallApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatvBallApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
